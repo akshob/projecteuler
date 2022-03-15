ac3feac68efc9122b070da1c5e1f6da2749b6ba4
Akshob Gopalakrishna <info@akshob.in>
2022-03-15 15:04:52 -0700

    create maven project

---
+++ src/main/java/in/akshob/projecteuler/ProjectEuler.java
@@ -0,0 +1,13 @@
+package in.akshob.projecteuler;
+
+/**
+ * Hello world!
+ *
+ */
+public class ProjectEuler 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/in/akshob/projecteuler/ProjectEulerTest.java
@@ -0,0 +1,20 @@
+package in.akshob.projecteuler;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ProjectEulerTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
