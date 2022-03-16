1f42c3d1e590be5cf290d7dbcf58307b6e8b71d4
Akshob Gopalakrishna <info@akshob.in>
2022-03-16 13:18:53 -0700

    add problem 3 in main app

--- src/main/java/in/akshob/projecteuler/ProjectEuler.java
+++ src/main/java/in/akshob/projecteuler/ProjectEuler.java
@@ -2,6 +2,7 @@ package in.akshob.projecteuler;
 
 import in.akshob.projecteuler.problems.Fibonacci;
 import in.akshob.projecteuler.problems.MultiplesOf;
+import in.akshob.projecteuler.problems.PrimeFactor;
 
 
 public class ProjectEuler 
@@ -11,5 +12,6 @@ public class ProjectEuler
         int i = 0;
         System.out.printf("Problem %d: %d\n", ++i, new MultiplesOf().findSum(1000));
         System.out.printf("Problem %d: %d\n", ++i, new Fibonacci().findEvenSum(4000000));
+        System.out.printf("Problem %d: %d\n", ++i, new PrimeFactor().largestFactor(600851475143L));
     }
 }
